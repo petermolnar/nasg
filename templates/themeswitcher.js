534daff81a4f420fca0efb1bd72269c4bb79e4b9
Peter Molnar <hello@petermolnar.eu>
2018-11-15 14:06:35 +0000

    themeswitcher v3

--- templates/themeswitcher.js
+++ templates/themeswitcher.js
@@ -1,47 +1,58 @@
 var DEFAULT_THEME = 'dark';
 var ALT_THEME = 'light';
+var STORAGE_KEY = 'theme';
+var colorscheme = document.getElementsByName('colorscheme');
 
-function setTheme(mode) {
-    var st = document.querySelector('#css_alt');
-    var cb = document.querySelector('#contrast');
-    if (mode == DEFAULT_THEME) {
-        st.setAttribute("media", "speech");
-        cb.checked = true;
+function setTheme(e) {
+    var mode = e.target.value;
+    if (mode == 'auto') {
+        localStorage.removeItem(STORAGE_KEY);
     }
     else {
-        st.setAttribute("media", "all");
-        cb.checked = false;
+        localStorage.setItem(STORAGE_KEY, mode);
     }
+    applyTheme(mode);
 }
 
-function toggleTheme(e) {
-    var mode = DEFAULT_THEME;
-    if (e.checked == false) {
-        mode = ALT_THEME;
+function applyTheme(mode) {
+    var st = document.getElementById('css_alt');
+    if (mode == ALT_THEME) {
+        st.setAttribute('media', 'all');
     }
-    setTheme(mode);
-    localStorage.setItem("theme", mode);
-    return true;
+    else {
+        st.setAttribute('media', 'speech');
+    }
+
+    for(var i = colorscheme.length; i--; ) {
+        if(colorscheme[i].value == mode) {
+            colorscheme[i].checked = true;
+        }
+    }
 }
 
 function mqlTheme(e) {
-    console.log(e);
+    if (localStorage.getItem(STORAGE_KEY) != null) {
+        return false;
+    }
     if (e.matches) {
-        setTheme(ALT_THEME);
+        applyTheme(ALT_THEME);
     }
     else {
-        setTheme(DEFAULT_THEME);
+        applyTheme(DEFAULT_THEME);
     }
 }
 
-var theme = localStorage.getItem("theme");
-if (theme != null) {
-    setTheme(theme);
+var current = localStorage.getItem(STORAGE_KEY);
+if (current == null) { current = 'auto'; }
+applyTheme(current);
+
+var mql = window.matchMedia('(prefers-color-scheme: ' + ALT_THEME + ')');
+mql.addListener(mqlTheme);
+for(var i = colorscheme.length; i--; ) {
+    colorscheme[i].onclick = setTheme;
 }
-else {
-    var mql = window.matchMedia("(prefers-color-scheme: " + ALT_THEME + ")");
-    if(mql.matches) {
-        setTheme(ALT_THEME);
-    }
-    mql.addListener(mqlTheme);
+
+var themeforms = document.getElementsByClassName(STORAGE_KEY);
+for(var i = themeforms.length; i--; ) {
+    themeforms[i].style.display = 'inline-block';
 }
