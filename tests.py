a57fac3b11a84fc4f7fac77a1fc0a4bc5b320de0
Peter Molnar <hello@petermolnar.eu>
2018-12-01 10:45:27 +0000

    adding first set of tests

---
+++ tests.py
@@ -0,0 +1,54 @@
+import unittest
+import pandoc
+import exiftool
+import nasg
+import os
+import json
+
+class TestNASG(unittest.TestCase):
+    def test_url2slug(self):
+        i = 'http://boffosocko.com/2017/10/28/content-bloat-privacy-archives-peter-molnar/'
+        o = 'boffosockocom20171028content-bloat-privacy-archives-peter-molnar'
+        self.assertEqual(nasg.url2slug(i), o)
+
+class TestSingular(unittest.TestCase):
+    singular = nasg.Singular('tests/index.md')
+
+    # TODO: WTF?
+    #def test_category(self):
+        #print(self.singular.category)
+        #self.assertEqual(self.singular.category, 'tests')
+
+    #def test_files(self):
+
+        #self.assertEqual(
+            #self.singular.files,
+            #['/home/pemolnar/Projects/petermolnar.net/nasg/tests/tests.jpg']
+
+    def test_is_photo(self):
+        self.assertTrue(self.singular.is_photo)
+
+    def test_is_front(self):
+        self.assertFalse(self.singular.is_front)
+
+    def test_singular_tags(self):
+        self.assertEqual(
+            self.singular.tags,
+            ['Llyn Idwal', 'winter', 'spring', 'cloudy', 'Snowdonia', 'mountain', 'clouds', 'lake', 'mountains', 'snow', 'Wales', 'water']
+        )
+
+class TestExiftool(unittest.TestCase):
+    def test_exiftool(self):
+        with open('tests/.tests.jpg.json', 'rt') as expected:
+            o = json.loads(expected.read())
+        self.assertEqual(exiftool.Exif('tests/tests.jpg'), o)
+
+class TestPandoc(unittest.TestCase):
+    def test_pandoc(self):
+        i = '_this_ is a **test** string for [pandoc](https://pandoc.org)'
+        o = '<p><em>this</em> is a <strong>test</strong> string for <a href="https://pandoc.org">pandoc</a></p>'
+        self.assertEqual(pandoc.Pandoc(i), o)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/index.md
@@ -0,0 +1,29 @@
+---
+GPSLatitude: 53.115584
+GPSLongitude: -4.030248
+published: 2016-07-23T09:00:23+0000
+tags:
+- Llyn Idwal
+- winter
+- spring
+- cloudy
+- Snowdonia
+- mountain
+- clouds
+- lake
+- mountains
+- snow
+- Wales
+- water
+title: Llyn Idwal
+---
+
+![](tests.jpg)
+
+One can always wish for the perfect weather, yet most of the times, it remains a wish. Not this time.
+
+There is a National Trust recommendation to visit Llyn Idwal, which Nora[^1] spotted, and they were right about the place: it's magnificent. We really weren't expecting snow on the mountains in May, but that, combined with the clouds and the late afternoon sunshine was just perfect.
+
+I cropped the image, but apart from that, it really looked like this. No edits, no nothing.
+
+[^1]: https://norahamucska.eu
