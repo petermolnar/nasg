9eccd3e683a40e129ca891d8136a94cc6647d9cb
Peter Molnar <hello@petermolnar.eu>
2019-04-10 09:25:05 +0100

    example keys.py file

---
+++ keys.dist.py
@@ -0,0 +1,10 @@
+webmentionio = {
+    'domain': 'example.com',
+    'token': 'https://webmention.io/settings -> API key',
+    'secret': 'https://webmention.io/settings/webhooks -> callback secret'
+}
+
+telegraph = {
+        'token': 'https://telegraph.p3k.io/ -> select site under avatar -> settings gear icon -> API key'
+}
+
